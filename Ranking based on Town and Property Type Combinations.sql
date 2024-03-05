9df7549e88e943ed86f0a722a00cfebd41cc0113
Julio Antonio <julioam766@gmail.com>
2024-03-06 00:38:14 +0700

    modified:   Ranking based on Town and Property Type Combinations.sql

--- Ranking based on Town and Property Type Combinations.sql
+++ Ranking based on Town and Property Type Combinations.sql
@@ -1,4 +1,8 @@
+SELECT RANK() OVER(ORDER BY town ASC, property_type ASC) AS rank_order, town, property_type, customer_id, order_id
+FROM (
 SELECT c.town, c.property_type, o.customer_id, o.order_id
 FROM customer_preferences c 
 LEFT JOIN order_info o 
-ON c.customer_id = o.order_id
+ON c.customer_id = o.customer_id
+) AS t1 
+ORDER BY rank_order, customer_id, order_id
